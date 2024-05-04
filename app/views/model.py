fd5edd53b97b1208c279a308b1e1d90f12d6ac41
dan7258 <italyevdm03@gmail.com>
2024-05-04 16:54:19 +0500

    add authentication in get-models and get-pims

--- app/views/model.py
+++ app/views/model.py
@@ -1,22 +1,32 @@
 import uuid
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from app.services.database import get_db
-
+from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.schemas import ModelList, ProcessedImagesSchema, ModelSchema, ProcessedImagesList
 from ..models import ProcessedImages, Models
+from ..models import User as UserModel
+from app.deps import get_current_user
 
 model_router = APIRouter(prefix="/model", tags=["model"])
 
 
 @model_router.get("/get-models", response_model=ModelList)
-async def get_models(db: AsyncSession = Depends(get_db)):
+async def get_models(db: AsyncSession = Depends(get_db), check_user: UserModel = Depends(get_current_user)):
+    if not check_user:
+        raise HTTPException(
+            check_user
+        )
     models = await Models.get_all(db)
     return {"models": models}
 
 
 @model_router.get("/get-pims", response_model=ProcessedImagesList)
-async def get_pims(user_id: uuid.UUID, db: AsyncSession = Depends(get_db)):
+async def get_pims(user_id: uuid.UUID, db: AsyncSession = Depends(get_db), check_user: UserModel = Depends(get_current_user)):
+    if not check_user:
+        raise HTTPException(
+            check_user
+        )
     pims = await ProcessedImages.get_by_user_id(db,user_id)
     return {"pims": pims}
